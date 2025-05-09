b7c399ebc5abb1afcc2a542b7efd37d57402c941
Djema-merouane <djema_noureddine.fs@univ-boumerdes.dz>
2025-05-09 20:24:29 +0100

    videoSteam complet
    responsive up to 10 slot per div

--- smart_parking/static/js/videoStream.js
+++ smart_parking/static/js/videoStream.js
@@ -2,6 +2,7 @@ import { scheduleStaticTokenRefresh } from "./utils.js";
 let swiper, w, h;
 let areaLength;
 const indexToAreaIdMap = {};
+let MaxareaTop = 0;
 
 //calls
 const areaList = document.getElementById("area-list");
@@ -77,6 +78,15 @@ document.addEventListener("DOMContentLoaded", async () => {
       parking_slots_holder.classList.add("parking-slots-holder");
       parking_slots_holder.id = `holders-${areaId}-${i + 1}`;
       parking_container.appendChild(parking_slots_holder);
+      if (i == 0) {
+        const parking_way = document.createElement("div");
+        parking_way.classList.add("parking-way");
+        parking_way.id = `entry-way-${areaId}`;
+        parking_container.appendChild(parking_way);
+        const parking_way_2 = document.createElement("div");
+        parking_way_2.classList.add("parking-way");
+        parking_container.appendChild(parking_way_2);
+      }
     }
 
     const wschema = window.location.protocol === "https:" ? "wss" : "ws";
@@ -124,21 +134,22 @@ document.addEventListener("DOMContentLoaded", async () => {
           p.textContent = `${i + 1}`;
           parking_slots.appendChild(p);
           // parking_slots.textContent = `${i + 1}`;
-          if (i < areaLength / 2) {
+          if (i < areaLength / 2 || areaLength < 6) {
             parking_slots_holder_target_1.appendChild(parking_slots);
+            MaxareaTop = MaxareaTop + 1;
           } else {
             parking_slots_holder_target_2.appendChild(parking_slots);
           }
         }
       }
-      w = document.getElementById(`parkingspace-${areaId}`).offsetWidth;
-      h = document.getElementById(`parkingspace-${areaId}`).offsetHeight;
+
       if (data.spot_details) {
         data.spot_details.forEach((spot) => {
           let number = spot.spot.match(/\d+/)[0] - 1;
           const slot = parseInt(number);
           const isOccupied = spot.status === "occupied";
-
+          w = document.getElementById(`parkingspace-${areaId}`).offsetWidth;
+          h = document.getElementById(`parkingspace-${areaId}`).offsetHeight;
           if (isOccupied && !document.getElementById(`car-${areaId}-` + slot)) {
             carenter(areaId, slot, areaLength);
           } else if (
@@ -171,7 +182,7 @@ function carexit(areaId, slot, areaLength) {
   const slott = document.getElementById(`slot-${areaId}-${slot}`);
   const pElement = slott.querySelector("p");
   if (pElement) {
-    pElement.textContent = ""; // Set the text content of the <p> element to nothing
+    pElement.textContent = "" + (slot + 1); // Set the text content of the <p> element to nothing
   }
   document.getElementById(`car-${areaId}-` + slot.toString()).style.position =
     "absolute";
@@ -190,6 +201,11 @@ function carexit(areaId, slot, areaLength) {
   const car = document.getElementById(`car-${areaId}-${slot}`);
   car.addEventListener("animationend", function () {
     car.remove();
+    const slott = document.getElementById(`slot-${areaId}-${slot}`);
+    const pElement = slott.querySelector("p");
+    if (pElement) {
+      pElement.textContent = "" + (slot + 1); // Set the text content of the <p> element to nothing
+    }
   });
 }
 
@@ -200,75 +216,58 @@ function generatenewcar(areaId, slot, areaLength) {
   img.src = window.CAR_IMAGE_URL;
   img.className = "new-car-origin";
   img.id = `car-${areaId}-${slot}`;
-  img.style.width = ((w * 0.2) / window.innerWidth) * 100 + "vw";
+  img.style.width = ((w * 0.14) / window.innerWidth) * 100 + "vw";
   const anim = document.createElement("style");
-  let totalOffset, verticalOffset;
-  let rotation = "90deg";
-
-  if (slot < areaLength / 2) {
-    // Left-side slots (0–4)
-    img.style.left = "12%";
-
-    // if (slot == areaLength / 2 - 1) {
-    //   verticalOffset += h * 0.0012;
-    //
-    const screenSizes = [
-      { maxWidth: 600, totalOffset: "7vh", verticalOffsetFactor: 0.83 },
-      { maxWidth: 800, totalOffset: "9.4vh", verticalOffsetFactor: 0.809 },
-      { maxWidth: 1000, totalOffset: "13vh", verticalOffsetFactor: 0.8 },
-      { maxWidth: 1100, totalOffset: "16vh", verticalOffsetFactor: 0.77 },
-      { maxWidth: 1300, totalOffset: "18vh", verticalOffsetFactor: 0.77 },
-      { maxWidth: 1500, totalOffset: "22vh", verticalOffsetFactor: 0.75 },
-      { maxWidth: 1700, totalOffset: "25vh", verticalOffsetFactor: 0.73 },
-    ];
-
-    for (let size of screenSizes) {
-      if (window.matchMedia(`(max-width: ${size.maxWidth}px)`).matches) {
-        totalOffset = size.totalOffset;
-        verticalOffset =
-          h * (size.verticalOffsetFactor + 0.11 * (slot * (areaLength / 2)));
-        break;
-      }
-    }
-
-    if (!totalOffset) {
-      totalOffset = "30vh";
-      verticalOffset = h * (0.73 + 0.11 * (slot * (areaLength / 2)));
-    }
+  img.style.right = "0%";
+  const baseFactor = 730;
+  const decrement = 170;
+  if (slot < areaLength / 2 || areaLength < 6) {
+    // Top-side slots
+    const adjustedFactor = baseFactor - slot * decrement;
 
     const rule = document.createTextNode(
       `@-webkit-keyframes car-park-${areaId}-${slot} {
-        0% {
-          transform: translate(0, 0) rotate(0deg);
-        }
-        70% {
-          transform: translate(0, -${verticalOffset}px) rotate(0deg);
-        }
+        from { 
+          transform: rotate(270deg);
+        } 
         80% {
-          transform: translate(0, -${verticalOffset}px) rotate(${rotation});
+          transform: rotate(270deg) translate(0px,-${adjustedFactor}%) 
+        }
+        90% {
+          transform: rotate(270deg) translate(0px,-${adjustedFactor}%) rotate(90deg);
         }
         to {
-          transform: translate(0, -${verticalOffset}px) rotate(${rotation}) translate(0, -${totalOffset});
-        }
+ transform: rotate(270deg) translate(0px,-${adjustedFactor}%) rotate(90deg) translate(0px,-${
+        h * 0.28
+      }px);
+        } 
       }`
     );
 
     anim.appendChild(rule);
     const exitAnim = document.createTextNode(`
       @-webkit-keyframes car-exit-top-${areaId}-${slot} {
-        from{
-          transform: translate(0, -${verticalOffset}px) rotate(${rotation}) translate(0, -${totalOffset});
-        }
-        60% {
-          transform: translate(0, -${verticalOffset}px) rotate(${rotation}) translate(0, -${totalOffset}) translate(0, 110%); ;
-        }
+        from { 
+          transform: rotate(270deg) translate(0px,-${adjustedFactor}%) rotate(90deg) translate(0px,-${
+      h * 0.3
+    }px);
+        } 
         80% {
-          transform: translate(0, -${verticalOffset}px) rotate(${rotation}) translate(0, -${totalOffset}) translate(0, 110%) rotate(${rotation});
+          transform: rotate(270deg) translate(0px,-${adjustedFactor}%) rotate(90deg) translate(0px,-${
+      h * 0.3
+    }px) translateY(${h * 0.3}px) ;
+        }
+        90% {
+          transform: rotate(270deg) translate(0px,-${adjustedFactor}%) rotate(90deg) translate(0px,-${
+      h * 0.3
+    }px) translateY(${h * 0.3}px) rotate(90deg) ;
         }
         to {
-          transform: translate(0, -${verticalOffset}px) rotate(${rotation}) translate(0, -${totalOffset}) translate(0, 110%) rotate(${rotation}) translate(0, -${
-      verticalOffset * 2
-    }px);
+          transform: rotate(270deg) translate(0px,-${adjustedFactor}%) rotate(90deg) translate(0px,-${
+      h * 0.3
+    }px) translateY(${
+      h * 0.3
+    }px) rotate(90deg)  translate(0px, -${adjustedFactor}%) ;
         }
       }
     `);
@@ -280,61 +279,59 @@ function generatenewcar(areaId, slot, areaLength) {
       }
 
       img.style.position = "relative";
-      img.style.transform = `rotate(90deg)`;
-      img.style.right = "0";
+      img.style.transform = `rotate(0deg)`;
       img.style.animation = "";
 
       window.addEventListener("resize", () => {
-        img.style.width = ((w * 0.2) / window.innerWidth) * 100 + "vw";
+        img.style.width = ((w * 0.15) / window.innerWidth) * 100 + "vw";
       });
     });
   } else {
-    // Right-side slots (5–9)
-
-    img.style.right = "12%";
-    const screenSizes = [
-      { maxWidth: 600, totalOffset: "8vh", verticalOffsetFactor: 0.72 },
-      { maxWidth: 800, totalOffset: "10.6vh", verticalOffsetFactor: 0.7 },
-      { maxWidth: 1000, totalOffset: "15vh", verticalOffsetFactor: 0.69 },
-      { maxWidth: 1100, totalOffset: "18vh", verticalOffsetFactor: 0.66 },
-      { maxWidth: 1300, totalOffset: "20vh", verticalOffsetFactor: 0.66 },
-      { maxWidth: 1500, totalOffset: "25vh", verticalOffsetFactor: 0.64 },
-      { maxWidth: 1700, totalOffset: "28vh", verticalOffsetFactor: 0.62 },
-    ];
-
-    for (let size of screenSizes) {
-      if (window.matchMedia(`(max-width: ${size.maxWidth}px)`).matches) {
-        totalOffset = size.totalOffset;
-        verticalOffset =
-          h * (size.verticalOffsetFactor + 0.11 * (slot * (areaLength / 2)));
-        break;
-      }
-    }
-
-    if (!totalOffset) {
-      totalOffset = "33vh";
-      verticalOffset = h * (0.62 + 0.11 * (slot * (areaLength / 2)));
-    }
-    // if (slot == areaLength - 1) {
-    //   verticalOffset += h * 0.0012;
-    // }
+    // Bottom-side slots
+    const adjustedFactor = baseFactor - (slot - areaLength / 2) * decrement;
     const rule = document.createTextNode(
       `@-webkit-keyframes car-park-${areaId}-${slot} {
-        0% { transform: translateY(0) rotate(0deg); }
-        70% { transform: translateY(-${verticalOffset}px) rotate(0deg); }
-        80% { transform: translateY(-${verticalOffset}px) rotate(-90deg); }
-        100% { transform: translateY(-${verticalOffset}px) rotate(-90deg) translateY(-${totalOffset});}
+        from { 
+          transform: rotate(-90deg);
+        } 
+        80% {
+          transform: rotate(-90deg) translate(0px,-${adjustedFactor}%) ;
+        }
+        90% {
+          transform: rotate(-90deg) translate(0px,-${adjustedFactor}%)  rotate(-90deg);
+        }
+        to {
+          transform: rotate(-90deg) translate(0px,-${adjustedFactor}%) rotate(-90deg) translate(0px,-${
+        h * 0.3
+      }px);
+        } 
       }`
     );
     anim.appendChild(rule);
     const exitAnim = document.createTextNode(
       `@-webkit-keyframes car-exit-bottom-${areaId}-${slot} {
-        0% { transform: translateY(-${verticalOffset}px) rotate(-90deg) translateY(-${totalOffset}); }
-        60% { transform: translateY(-${verticalOffset}px) rotate(-90deg) translateY(-${totalOffset}) translateY(110%);}
-        80% { transform: translateY(-${verticalOffset}px) rotate(-90deg) translateY(-${totalOffset})  translateY(110%) rotate(-${rotation}); }
-        100% { transform: translateY(-${verticalOffset}px) rotate(-90deg) translateY(-${totalOffset}) translateY(110%) rotate(-${rotation}) translateY(-${
-        verticalOffset * 2
-      }px);}
+        from { 
+           transform: rotate(-90deg) translate(0px,-${adjustedFactor}%) rotate(-90deg) translate(0px,-${
+        h * 0.3
+      }px);
+        } 
+        80% {
+           transform: rotate(-90deg) translate(0px,-${adjustedFactor}%) rotate(-90deg) translate(0px,-${
+        h * 0.3
+      }px) translate(0px,${h * 0.3}px);;
+        }
+        90% {
+           transform: rotate(-90deg) translate(0px,-${adjustedFactor}%) rotate(-90deg) translate(0px,-${
+        h * 0.3
+      }px) translate(0px,${h * 0.3}px) rotate(-90deg);
+        }
+        to {
+           transform: rotate(-90deg) translate(0px,-${adjustedFactor}%) rotate(-90deg) translate(0px,-${
+        h * 0.3
+      }px) translate(0px,${
+        h * 0.3
+      }px) rotate(-90deg) translate(0px,-${adjustedFactor}%);
+        } 
       }`
     );
     anim.appendChild(exitAnim);
@@ -345,7 +342,7 @@ function generatenewcar(areaId, slot, areaLength) {
       }
 
       img.style.position = "relative";
-      img.style.transform = `translateX(-80%) rotate(-90deg)`;
+      img.style.transform = "rotate(180deg)";
       img.style.animation = "";
 
       // Only adjust the width on resize, not position
@@ -363,29 +360,19 @@ function generatenewcar(areaId, slot, areaLength) {
 
 // Function to handle car entry animation and creation
 function carenter(areaId, slot, areaLength) {
-  if (!document.getElementById(`car-${areaId}-` + slot.toString())) {
+  const carId = `car-${areaId}-${slot}`;
+  if (!document.getElementById(carId)) {
     generatenewcar(areaId, slot, areaLength);
-    document.getElementById(
-      `slot-${areaId}-` + slot.toString()
-    ).style.background = "rgb(146,18,18)";
-    if (slot != areaLength / 2 && slot != areaLength)
-      document.getElementById(`car-${areaId}-${slot}`).style.right =
-        -w +
-        w * 0.04 +
-        (5 - ((slot + 1) % 5)) * (w * 0.8 * 0.2) +
-        w * 0.8 * 0.05 +
-        "px";
-    else
-      document.getElementById(`car-${areaId}-${slot}`).style.right =
-        -w + w * 0.04 + w * 0.8 * 0.05 + "px";
-    if (slot < areaLength)
-      document.getElementById(
-        `car-${areaId}-${slot}`
-      ).style.animation = `car-park-${areaId}-${slot} 2s both`;
-    else
-      document.getElementById(
-        `car-${areaId}-${slot}`
-      ).style.animation = `car-park-${areaId}-${slot} 2s both`;
+    const isTopRow = slot < areaLength / 2;
+    const slotsPerRow = Math.ceil(areaLength / 2);
+
+    document.getElementById(`slot-${areaId}-${slot}`).style.background =
+      "rgb(146,18,18)";
+
+    const carElement = document.getElementById(carId);
+    if (!carElement) return;
+
+    carElement.style.animation = `car-park-${areaId}-${slot} 2s both`;
   } else {
     carexit(areaId, slot, areaLength);
   }
